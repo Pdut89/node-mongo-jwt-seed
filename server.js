bc6d966fb5587dda6d15d43f6b0f310f40be4dc9
Pieter <pdutoit@fullfacing.com>
2020-06-03 09:55:18 +0200

    updated lock

--- server.js
+++ server.js
@@ -1,12 +1,45 @@
+require('dotenv').config()
+
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
+const { 
+  PORT, 
+  MONGO_URL, 
+  DB_NAME 
+} = process.env
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+mongoose.connect(`${MONGO_URL}/${DB_NAME}`, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+}) 
+ 
+const User = mongoose.model('Stealing', {
+  name: {
+    type: String,
+  },
+  age: {
+    type: Number
+  }
 })
 
-app.listen(3000)
+const me = new User({
+  name: 'Pieter',
+  age: 30
+})
 
-const port = process.env.PORT;
-console.log(port)
+me.save().then(() => {
+  console.log(me)
+}).catch(err => {
+  console.log(err)
+})
+
+app.get('/', function (req, res) {
+  res.send('Remax MongoDB Server')
+})
+
+app.listen(PORT, () => {
+  console.log(`App is running on PORT:${PORT}`)
+})
+
