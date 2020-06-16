4018a4b27a2e5aca454b4a1e0f6a7211c6120590
Pieter <pdutoit@fullfacing.com>
2020-06-16 21:10:33 +0200

    add cors

--- src/server.js
+++ src/server.js
@@ -1,13 +1,23 @@
 require('dotenv').config()
 require('./db/mongoose.js')
 
-const { PORT } = process.env
+const { 
+  PORT,
+  CORS_ORIGIN_URL
+} = process.env 
+
+const corsConfig = {
+  origin: CORS_ORIGIN_URL,
+  optionsSuccessStatus: 200
+}
 
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 const userRouter = require('./routers/user')
 
+app.use(cors(corsConfig))
 app.use(express.json())
 app.use(userRouter)
 
